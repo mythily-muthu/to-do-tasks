a3ad5c26fe82eb485126758da6d774ababab0721
mythily-muthu <mythilimuthu18@gmail.com>
2023-04-19 14:49:46 +0530

    Added global css

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import InputArea from './Components/InputArea';
 
 function App() {
   return (
@@ -7,7 +8,7 @@ function App() {
       <div className="title">
         <h1>To-Do-Tasks</h1>
       </div>
-
+      <InputArea />
     </div>
   );
 }
