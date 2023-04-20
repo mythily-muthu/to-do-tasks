5587c679cf29cb57fc342c621bdbcf7bc7dca4ab
mythily-muthu <mythilimuthu18@gmail.com>
2023-04-20 17:26:04 +0530

    added color in background

--- src/Components/InputArea.js
+++ src/Components/InputArea.js
@@ -1,8 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
+import "./InputArea.css"
+const InputArea = (props) => {
+    const [inputText, setInputText] = useState("")
 
-const InputArea = () => {
+    const handleChange = (event) => {
+        setInputText(event.target.value)
+
+    }
     return (
-        <div>InputArea</div>
+        <div className='form'>
+            <input className='input' type='text' onChange={handleChange} value={inputText} />
+            <button className='button' onClick={() => {
+                props.addItems(inputText);
+                setInputText("");
+            }}>
+                <span className='span'>ADD</span>
+            </button>
+        </div>
     )
 }
 
